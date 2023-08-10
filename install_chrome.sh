502ac91cfc6c722d4fd4f65b202b5bd54f776a7e
Yurii <yuriydorosh2005@gmail.com>
2023-08-10 17:55:18 +0300

    ADD: bash file for installing Google Chrome

---
+++ install_chrome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt update
+sudo apt --only-upgrade install google-chrome-stable
